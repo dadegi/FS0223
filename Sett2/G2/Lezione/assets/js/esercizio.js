51b032a12fd72b034c26fa5ae65c3abb13feeb58
Naviganet <dadegi@gmail.com>
2023-04-05 10:51:20 +0200

    Codici settimana 2 giorno 3

---
+++ Sett2/G2/Lezione/assets/js/esercizio.js
@@ -0,0 +1,16 @@
+var nome1 = 'john';
+var nome2 = 'John';
+
+console.log(nome1 == nome2);
+
+if(nome1 == nome2) {
+    console.log('Le variabili sono uguali');
+}else {
+    console.log('Le variabili non sono uguali');
+}
+
+if(nome1.toLowerCase == nome2.toLowerCase) {
+    console.log('Le variabili sono uguali');
+}else {
+    console.log('Le variabili non sono uguali');
+}
