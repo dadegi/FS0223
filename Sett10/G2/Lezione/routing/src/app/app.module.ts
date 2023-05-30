61a84ff86c3bb7400ef3b3b4175b6dccdfc11165
Naviganet <dadegi@gmail.com>
2023-05-30 12:39:00 +0200

    Codice lezione settimana 10 giorno 2 (b) - Angular

--- Sett10/G2/Lezione/routing/src/app/app.module.ts
+++ Sett10/G2/Lezione/routing/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Route, RouterModule } from '@angular/router';
+import { Route, RouterModule } from '@angular/router'; // Importazione dell'interfaccia Route e del modulo RouterModule per gestire la navigazione (routing)
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -10,10 +10,10 @@ import { TerzoComponent } from './components/terzo/terzo.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 
-const routes: Route[] = [
+const routes: Route[] = [ // Creazione di una costante basata sull'interfaccia Route che contiene i percorsi di attivazione dei componenti
     {
-        path: '',
-        component: HomeComponent
+        path: '', // Percorso che sarà indicato nel link
+        component: HomeComponent // Componente che si attiverà al click sul link
     },
     {
         path: 'primo',
@@ -41,7 +41,7 @@ const routes: Route[] = [
     ],
     imports: [
         BrowserModule,
-        RouterModule.forRoot(routes)
+        RouterModule.forRoot(routes) // Dichiarazione di import del modulo RouterModule con il metodo forRoot (per gestire le rotte) che riceve l'array di rotte definito prima
     ],
     providers: [],
     bootstrap: [AppComponent],
